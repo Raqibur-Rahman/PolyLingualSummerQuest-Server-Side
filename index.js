ffc98c479ce1efe9e9ff71e1a11a8688271608d7
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-06-14 15:05:58 +0600

    connected with mongodb and server is ok

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.btjmiui.mongodb.net/?retryWrites=true&w=majority`;
+
+// Middleware
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.use(express.json());
+
+console.log(process.env.DB_PASS);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
+});
+
+async function run() {
+  try {
+ 
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const instructorsCollection = client.db('PolyLingualDB').collection('InstructorsCollections');
+
+    app.get('/instructors',async(req,res)=>{
+      const result=await instructorsCollection.find().toArray();
+      res.send(result);
+    })
+    
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  } finally {
+    
+  }
+}
+
+run().catch(console.dir);
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.send("Server is running");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
